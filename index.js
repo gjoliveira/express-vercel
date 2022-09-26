dcbdb5d4ab2f83368c86761ded7c49017fa3666b
gjoliveira <gjoliveira.email@gmail.com>
2022-09-26 14:23:29 -0300

    commit

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
-const express = require("express");
 const app = express();
-
+const cron = require("node-cron");
+const express = require("express");
 const PORT = process.env.PORT || 8080;
+const { Curl } = require("node-libcurl");
 
 app.get("/", (req, res) => res.send("Hello World!"));
+
+cron.schedule("* * * * *", () => {
+  const curl = new Curl();
+
+  curl.setOpt("URL", "esites.eco.br/chats/cron_job/publish_post");
+  curl.setOpt("FOLLOWLOCATION", true);
+
+  curl.on("end", function (statusCode, data) {
+    console.info(statusCode);
+    console.info("---");
+    console.info(data.length);
+    console.info("---");
+    console.info(this.getInfo("TOTAL_TIME"));
+
+    this.close();
+  });
+
+  curl.perform();
+});
 
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
