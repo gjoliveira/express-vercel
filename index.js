f6bd3e23e9107699951dfb3212d264eff78f9eeb
gjoliveira <gjoliveira.email@gmail.com>
2022-09-26 14:20:51 -0300

    commit

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
